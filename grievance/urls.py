04e4e4ae56cbf987d3917c328d227dffae5e9697
VinayakER <vinayak.ervk@gmail.com>
2023-06-06 06:31:42 +0530

    feat(admin can assign ticket and update staus of ticket)

--- grievance/urls.py
+++ grievance/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ticketDetails, comment, ticketStatus
+from .views import ticketDetails, comment, ticketUpdate
 urlpatterns =[
     path('<int:id>',ticketDetails, name="ticketDetails"),
     path('<int:ticket_id>/comment', comment),
-    path('<int:ticket_id>/status/<int:value>', ticketStatus)
+    path('<int:id>/update', ticketUpdate)
 ]
--- grievance/views.py
+++ grievance/views.py
@@ -3,6 +3,8 @@ from .models import Grievance, Comment
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
+from account.models import User
 
 @login_required
 def grievance(request):
@@ -29,7 +31,8 @@ def make_complaint(request):
 def ticketDetails(request, id):
     ticket = Grievance.objects.get(id=id)
     comments = Comment.objects.filter(ticket=ticket)
-    return render(request,"grievance/ticketDetails.html", context={"ticket":ticket, "comments":comments})
+    users = User.objects.filter(Q(is_staff=True)&Q(is_superuser=False))
+    return render(request,"grievance/ticketDetails.html", context={"ticket":ticket, "comments":comments, "users":users})
 
 @login_required
 def comment(request, ticket_id):
@@ -44,10 +47,22 @@ def comment(request, ticket_id):
 
 
 @login_required
-def ticketStatus(request, ticket_id, value):
+def ticketUpdate(request, id):
+    ticket = Grievance.objects.get(id=id)
 
-    ticket = Grievance.objects.get(id=ticket_id)
-    ticket.status = value
-    ticket.save()
-    # comment = Comment()
-    return redirect(f"/ticket/{ticket_id}")
+    if request.method=="POST":
+        data = request.POST
+        status = data.get('status')
+        assigned_to = data.get('assigned_to')
+        if status and ticket.status!=int(status) and int(status) in [2,3,4]:
+            ticket.status=status
+        if assigned_to:
+            try:
+                assign_to_user = User.objects.get(id=assigned_to)
+                if ticket.assigned_to != assign_to_user and assign_to_user.is_staff==True and assign_to_user.is_superuser==False:
+                    ticket.assigned_to=assign_to_user
+            except User.DoesNotExist:
+                pass
+        ticket.save()
+
+    return redirect(f"/ticket/{id}")
